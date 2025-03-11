e76cb8138cbcb351f5aba3856d7a7678f8d1b775
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-11 08:48:24 +0530

    added the entity and role to the user Service

---
+++ user-service/src/main/java/com/compay/Inelligent_supply_chain/user_service/entities/User.java
@@ -0,0 +1,30 @@
+package com.compay.Inelligent_supply_chain.user_service.entities;
+
+
+import com.compay.Inelligent_supply_chain.user_service.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
---
+++ user-service/src/main/java/com/compay/Inelligent_supply_chain/user_service/enums/Role.java
@@ -0,0 +1,14 @@
+package com.compay.Inelligent_supply_chain.user_service.enums;
+
+public enum Role {
+
+    ADMIN,
+    CUSTOMER,
+    SUPPLIER,
+    WAREHOUSE_MANAGER,
+    DELIVERY_AGENT,
+    INVENTORY_MANAGER,
+    ACCOUNTANT,
+    ANALYST
+
+}
