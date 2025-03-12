dec19890de28dcce4bb9e827ce74b22b8c8a1e76
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-12 07:54:23 +0530

    added the repository for the inventory-repository

---
+++ Inventory-service/src/main/java/com/compay/Inelligent_supply_chain/Inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.compay.Inelligent_supply_chain.Inventory_service.repository;
+
+import com.compay.Inelligent_supply_chain.Inventory_service.entities.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
