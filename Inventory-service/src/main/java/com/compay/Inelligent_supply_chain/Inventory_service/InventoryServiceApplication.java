041d45b441abdf0095eda430eed328279ef27f02
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-12 07:42:05 +0530

    added the inventory-Management-Service

---
+++ Inventory-service/src/main/java/com/compay/Inelligent_supply_chain/Inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.compay.Inelligent_supply_chain.Inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
---
+++ Inventory-service/src/test/java/com/compay/Inelligent_supply_chain/Inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compay.Inelligent_supply_chain.Inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
