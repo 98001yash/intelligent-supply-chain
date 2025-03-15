54c6e91e1798778672b2a5645f6808ea0829b726
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-15 14:21:18 +0530

    added the shipment service in the application

---
+++ shipment-service/src/test/java/com/compay/Inelligent_supply_chain/shipment_service/ShipmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compay.Inelligent_supply_chain.shipment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
