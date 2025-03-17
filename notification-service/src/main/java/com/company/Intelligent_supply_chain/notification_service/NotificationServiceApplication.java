335ea0954dce6147ab077ea40bbd28620ab3664e
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-17 08:18:32 +0530

    added the notification service

---
+++ notification-service/src/main/java/com/company/Intelligent_supply_chain/notification_service/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.Intelligent_supply_chain.notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
