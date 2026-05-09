2dc95a5026bba80d5eb27ecee9a5ca899e3602af
98001yash <yashchauhan.gaya@gmail.com>
2026-05-09 14:21:45 +0530

    created the inventoryReservedEvebt

---
+++ shared-kafka-events/src/main/java/com/company/intelligent_supply_chain/events/InventoryReservedEvent.java
@@ -0,0 +1,21 @@
+package com.company.intelligent_supply_chain.events;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class InventoryReservedEvent extends BaseEvent{
+
+    private Long orderId;
+    private String skuCode;
+    private Integer quantity;
+    private boolean reserved;
+}
