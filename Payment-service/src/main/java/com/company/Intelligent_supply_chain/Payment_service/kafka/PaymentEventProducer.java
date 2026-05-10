87aa95471786fe7fbbfb3eefd574bf8e1e65cd71
98001yash <yashchauhan.gaya@gmail.com>
2026-05-10 18:16:59 +0530

    created the paymentEventProducer

---
+++ Payment-service/src/main/java/com/company/Intelligent_supply_chain/Payment_service/kafka/PaymentEventProducer.java
@@ -0,0 +1,27 @@
+package com.company.Intelligent_supply_chain.Payment_service.kafka;
+
+
+import com.company.intelligent_supply_chain.events.PaymentProcessedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentEventProducer {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void publishPaymentEventProcessedEvent(
+            PaymentProcessedEvent event
+    ){
+        kafkaTemplate.send("payment-processed-topic",
+                event.getOrderId().toString(),
+                event);
+
+        log.info("PaymentProcessedEvent published for order Id: {}",
+                event.getOrderId());
+    }
+}
