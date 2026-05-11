c6a0bdd7a5aeb6c3734e32f11f5c745e5845dd6d
98001yash <yashchauhan.gaya@gmail.com>
2026-05-11 07:54:47 +0530

    created the KafkaConsumerConfig

---
+++ order-service/src/main/java/com/company/Intelligent_supply_chain/order_service/config/KafkaConsumerConfig.java
@@ -0,0 +1,79 @@
+package com.company.Intelligent_supply_chain.order_service.config;
+
+
+import com.company.intelligent_supply_chain.events.PaymentProcessedEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, PaymentProcessedEvent>
+    consumerFactory() {
+
+        JsonDeserializer<PaymentProcessedEvent>
+                deserializer =
+                new JsonDeserializer<>(
+                        PaymentProcessedEvent.class
+                );
+
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeHeaders(false);
+        Map<String, Object> config =
+                new HashMap<>();
+
+        config.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9092"
+        );
+        config.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                "order-payment-group"
+        );
+        config.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class
+        );
+        config.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                JsonDeserializer.class
+        );
+        config.put(
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                "earliest"
+        );
+        return new DefaultKafkaConsumerFactory<>(
+                config,
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<
+            String,
+            PaymentProcessedEvent
+            > kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<
+                String,
+                PaymentProcessedEvent
+                > factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(
+                consumerFactory()
+        );
+        return factory;
+    }
+}
