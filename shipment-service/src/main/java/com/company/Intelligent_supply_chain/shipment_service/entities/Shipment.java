e356d1eca41aabd74d4a2ab74c288171fd834761
98001yash <yashchauhan.gaya@gmail.com>
2026-05-12 13:16:16 +0530

    created the shipment entity and updated the delivery Status enum

---
+++ shipment-service/src/main/java/com/company/Intelligent_supply_chain/shipment_service/entities/Shipment.java
@@ -0,0 +1,59 @@
+package com.company.Intelligent_supply_chain.shipment_service.entities;
+
+
+import com.company.Intelligent_supply_chain.shipment_service.enums.DeliveryStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "shipments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Shipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    private Long orderId;
+
+    private String trackingNumber;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    private LocalDateTime estimatedDeliveryTime;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+
+        if(this.trackingNumber == null) {
+            this.trackingNumber =
+                    "TRK-" + UUID.randomUUID()
+                            .toString()
+                            .substring(0,8)
+                            .toUpperCase();
+        }
+
+        if(this.status == null) {
+            this.status = DeliveryStatus.CREATED;
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
--- shipment-service/src/main/java/com/company/Intelligent_supply_chain/shipment_service/enums/DeliveryStatus.java
+++ shipment-service/src/main/java/com/company/Intelligent_supply_chain/shipment_service/enums/DeliveryStatus.java
@@ -1,10 +1,18 @@
 package com.company.Intelligent_supply_chain.shipment_service.enums;
 
+
 public enum DeliveryStatus {
 
+    CREATED,
+    PACKED,
+    READY_FOR_DISPATCH,
     ASSIGNED,
     PICKED_UP,
     IN_TRANSIT,
+    OUT_FOR_DELIVERY,
     DELIVERED,
-    CANCELLED
+    FAILED,
+    CANCELLED,
+    RETURN_INITIATED,
+    RETURNED
 }
