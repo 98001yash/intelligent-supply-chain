359baf84a6f253dbd019cd25b6cc3df002eece8f
98001yash <yashchauhan.gaya@gmail.com>
2026-05-13 07:10:42 +0530

    updated the shipment event consumer

--- shipment-service/src/main/java/com/company/Intelligent_supply_chain/shipment_service/kafka/ShipmentEventConsumer.java
+++ shipment-service/src/main/java/com/company/Intelligent_supply_chain/shipment_service/kafka/ShipmentEventConsumer.java
@@ -4,6 +4,7 @@ import com.company.Intelligent_supply_chain.shipment_service.entities.Shipment;
 import com.company.Intelligent_supply_chain.shipment_service.enums.DeliveryStatus;
 import com.company.Intelligent_supply_chain.shipment_service.repositories.ShipmentRepository;
 import com.company.intelligent_supply_chain.events.PaymentProcessedEvent;
+import com.company.intelligent_supply_chain.events.ShipmentCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -17,6 +18,7 @@ import java.time.LocalDateTime;
 public class ShipmentEventConsumer {
 
     private final ShipmentRepository shipmentRepository;
+    private final ShipmentEventProducer shipmentEventProducer;
 
     @KafkaListener(
             topics = "payment-processed-topic",
@@ -29,32 +31,36 @@ public class ShipmentEventConsumer {
         log.info("Received PaymentProcessedEvent for Order ID: {}",
                 event.getOrderId());
 
-        // IMPORTANT
-        // Only create shipment if payment successful
-
-        if(!"SUCCESS".equalsIgnoreCase(
+        // ONLY CREATE SHIPMENT IF PAYMENT SUCCESSFUL
+        if (!"SUCCESS".equalsIgnoreCase(
                 event.getPaymentStatus())) {
 
-            log.warn("Payment failed for Order ID: {}. Shipment will not be created.",
-                    event.getOrderId());
+            log.warn(
+                    "Payment failed for Order ID: {}. Shipment will not be created.",
+                    event.getOrderId()
+            );
 
             return;
         }
 
         // IDEMPOTENCY CHECK
+
         boolean shipmentExists =
                 shipmentRepository
                         .findByOrderId(event.getOrderId())
                         .isPresent();
 
-        if(shipmentExists) {
+        if (shipmentExists) {
 
-            log.warn("Shipment already exists for Order ID: {}",
-                    event.getOrderId());
+            log.warn(
+                    "Shipment already exists for Order ID: {}",
+                    event.getOrderId()
+            );
 
             return;
         }
 
+        // CREATE SHIPMENT
         Shipment shipment = Shipment.builder()
                 .orderId(event.getOrderId())
                 .status(DeliveryStatus.CREATED)
@@ -66,8 +72,31 @@ public class ShipmentEventConsumer {
         Shipment savedShipment =
                 shipmentRepository.save(shipment);
 
-        log.info("Shipment created successfully for Order ID: {} with Shipment ID: {}",
+        log.info(
+                "Shipment created successfully for Order ID: {} with Shipment ID: {}",
                 savedShipment.getOrderId(),
-                savedShipment.getShipmentId());
+                savedShipment.getShipmentId()
+        );
+
+        // CREATE EVENT
+        ShipmentCreatedEvent shipmentCreatedEvent =
+                ShipmentCreatedEvent.builder()
+                        .shipmentId(savedShipment.getShipmentId())
+                        .orderId(savedShipment.getOrderId())
+                        .trackingNumber(savedShipment.getTrackingNumber())
+                        .shipmentStatus(savedShipment.getStatus().name())
+                        .courierName("NOT_ASSIGNED")
+                        .build();
+
+        // PUBLISH EVENT
+        shipmentEventProducer
+                .publishShipmentCreatedEvent(
+                        shipmentCreatedEvent
+                );
+
+        log.info(
+                "ShipmentCreatedEvent published for Order ID: {}",
+                savedShipment.getOrderId()
+        );
     }
 }
