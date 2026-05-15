b66c82923585b00e98b3445eefe05d5475a97704
98001yash <yashchauhan.gaya@gmail.com>
2026-05-15 08:47:37 +0530

    updated the refundProcessedEvent

--- shared-kafka-events/src/main/java/com/company/intelligent_supply_chain/events/RefundProcessedEvent.java
+++ shared-kafka-events/src/main/java/com/company/intelligent_supply_chain/events/RefundProcessedEvent.java
@@ -1,25 +1,30 @@
 package com.company.intelligent_supply_chain.events;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @SuperBuilder
 public class RefundProcessedEvent extends BaseEvent {
 
     private Long returnRequestId;
 
     private Long orderId;
+
     private Double refundAmount;
 
-    //  SUCCESS / FAILED
+    // SUCCESS / FAILED
     private String refundStatus;
+
     private String refundMethod;
+
+    // NEW
+    private String skuCode;
+
+    private Integer quantity;
 }
