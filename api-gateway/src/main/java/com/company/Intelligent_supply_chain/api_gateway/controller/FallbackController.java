ebab6f024d2f9f22276660b2e5df2af443371fd0
98001yash <yashchauhan.gaya@gmail.com>
2026-05-17 07:12:27 +0530

    created the fallback controller to the api-gateway

---
+++ api-gateway/src/main/java/com/company/Intelligent_supply_chain/api_gateway/controller/FallbackController.java
@@ -0,0 +1,28 @@
+package com.company.Intelligent_supply_chain.api_gateway.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/fallback/payment")
+    public ResponseEntity<Map<String, String>> paymentFallback(){
+        Map<String, String> response = new HashMap<>();
+
+        response.put("message","Payment service is temporarily unavailable. Please try again later.");
+
+        response.put("status","SERVICE_UNAVAILABLE");
+
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(response);
+    }
+
+
+}
